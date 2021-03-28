45a0b6d15f69b0ddd045942d8bb366d61eb907e8
Your Name <you@example.com>
2021-03-28 16:27:13 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+  $(".sidenav").sidenav();
+  $(".materialboxed").materialbox();
+  $(".parallax").parallax();
+  $(".tabs").tabs();
+  $(".datepicker").datepicker();
+  $(".tooltipped").tooltip();
+  $(".scrollspy").scrollSpy();
+});
